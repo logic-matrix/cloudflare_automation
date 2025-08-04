81f3ada38d8859d4f604c173c9752f7b48faff2a
Tarikul69 <tarikulabir69@gmail.com>
2025-08-04 21:25:10 +0600

    Update

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
-from flask import Flask, json, render_template
+from flask import Flask, json, render_template, jsonify
+import requests
 
+from app.controllers.email_controller import send_email
 from app.controllers.workers_controller import get_worker_analytics 
   
 
@@ -18,6 +20,16 @@ def worker_table():
 @app.route('/cloudflare/worker-analytics', methods=['GET'])
 def worker_analytics_route():
     return get_worker_analytics()
+
+@app.route('/email', methods=['get'])
+def email_route():
+    #result = send_email()
+    if requests.status_code == 200:
+        send_email()
+        return jsonify({"message": "Email sent successfully"}), 200
+    else: 
+        return jsonify({"message":"Failed to send email"}), 500
+
 
 if __name__ == '__main__':
     app.run(debug=True)
